af601e57b71c9180c18b32bf992792076b8ecb67
MilesWessler <miles.nicholas87@gmail.com>
2016-03-27 23:27:02 -0500

    fixed logout bug

---
+++ AngelShare/AngelShare/Models/SeedDB/SeedUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AngelShare.Models.SeedDB
+{
+    public class SeedUser : DropCreateDatabaseAlways<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+           
+        
+        }
+    }
+}
