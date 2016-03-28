79f13085d4b88a26c94be2ddf9f657fd88f3c16b
MilesWessler <miles.nicholas87@gmail.com>
2016-03-27 22:17:41 -0500

    fixed database error

--- AngelShare/AngelShare/Migrations/Configuration.cs
+++ AngelShare/AngelShare/Migrations/Configuration.cs
@@ -1,32 +1,39 @@
+using AngelShare.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
 namespace AngelShare.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-
-    internal sealed class Configuration : DbMigrationsConfiguration<AngelShare.Models.MyDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<AngelShare.Models.ApplicationDbContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
-            ContextKey = "AngelShare.Models.MyDbContext";
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
-        protected override void Seed(AngelShare.Models.MyDbContext context)
+        protected override void Seed(AngelShare.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            //var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            //string[] roleNames = { "Admin", "Member" };
+            //IdentityResult roleResult;
+            //foreach (var roleName in roleNames)
+            //{
+            //    if (!RoleManager.RoleExists(roleName))
+            //    {
+            //        roleResult = RoleManager.Create(new IdentityRole(roleName));
+            //    }
+            //}
+            //var UserManger = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            //UserManger.AddToRole("761f978f-508e-4553-836e-784d52abb8e7", "Admin");
+            //var UserManger1 = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            //UserManger.AddToRole("38f32110-d711-4bbf-8ede-0574b7746afe", "Admin");
         }
     }
 }
+
